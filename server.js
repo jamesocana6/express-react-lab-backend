3959388f8bbf7d49942a694b6e423866371be48b
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-09-27 16:41:40 -0400

    backend is done

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT;
+const projects = require("./projects.json")
+const about = require("./about.json")
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.redirect("/projects");
+})
+
+app.get('/projects', (req, res) => {
+    try {
+        res.json(projects);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+app.get("/about", (req, res) => {
+    try {
+        res.json(about);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log("We are listening on PORT: ", PORT);
+})
